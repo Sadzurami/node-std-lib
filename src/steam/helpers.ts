87de2da0d14dccc347502a77ed86be7f608a48de
Sadzurami <sadzurami2036@gmail.com>
2024-12-14 14:38:16 +0300

    feat: add getSessionExpiryDate function to calculate session expiry from refresh tokens

--- src/steam/helpers.ts
+++ src/steam/helpers.ts
@@ -1,6 +1,22 @@
 import crypto from 'crypto';
 import os from 'os';
 
+import { Session } from './interfaces/session.interface';
+
+export function getSessionExpiryDate(session: Session): Date {
+  try {
+    const timestamp = Math.min(
+      decodeRefreshToken(session.WebRefreshToken).exp,
+      decodeRefreshToken(session.MobileRefreshToken).exp,
+      decodeRefreshToken(session.DesktopRefreshToken).exp,
+    );
+
+    return new Date(timestamp * 1000);
+  } catch (error) {
+    throw new Error('Failed to get session expiry date', { cause: error });
+  }
+}
+
 export function decodeRefreshToken(token: string) {
   try {
     const parts = token.split('.');
