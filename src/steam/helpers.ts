c1df5c628ef2e8c88544e41febb0caa7b0a78d7a
Sadzurami <sadzurami2036@gmail.com>
2024-12-14 14:51:50 +0300

    feat: add functions getSessionExpiryDate & decodeRefreshToken

--- src/steam/helpers.ts
+++ src/steam/helpers.ts
@@ -1,10 +1,10 @@
-import crypto from 'crypto';
-import os from 'os';
-
 import { Session } from './interfaces/session.interface';
 
 export function getSessionExpiryDate(session: Session): Date {
   try {
+    const date = new Date(session.ExpiryDate);
+    if (!isNaN(date.getTime())) return date;
+
     const timestamp = Math.min(
       decodeRefreshToken(session.WebRefreshToken).exp,
       decodeRefreshToken(session.MobileRefreshToken).exp,
@@ -29,59 +29,5 @@ export function decodeRefreshToken(token: string) {
     return payloadJson as { iss: string; sub: string; aud: string[]; exp: number };
   } catch (error) {
     throw new Error('Failed to decode refresh token', { cause: error });
-  }
-}
-
-export function createMachineName(username?: string): string {
-  const hash = crypto.createHash('sha1');
-  hash.update(username || os.hostname());
-
-  const sha1 = hash.digest();
-  const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-
-  let output = 'DESKTOP-';
-  for (let i = 0; i < 7; i++) output += CHARS[sha1[i] % CHARS.length];
-
-  return output;
-}
-
-export function createMachineId(username?: string): Buffer {
-  const id = username || os.hostname();
-
-  return Buffer.concat([
-    byteToBuffer(0),
-    stringToBuffer('MessageObject'),
-
-    byteToBuffer(1),
-    stringToBuffer('BB3'),
-    stringToBuffer(sha1(`SteamUser Hash BB3 ${id}`)),
-
-    byteToBuffer(1),
-    stringToBuffer('FF2'),
-    stringToBuffer(sha1(`SteamUser Hash FF2 ${id}`)),
-
-    byteToBuffer(1),
-    stringToBuffer('3B3'),
-    stringToBuffer(sha1(`SteamUser Hash 3B3 ${id}`)),
-
-    byteToBuffer(8),
-    byteToBuffer(8),
-  ]);
-
-  function sha1(input: string) {
-    const hash = crypto.createHash('sha1');
-    hash.update(input, 'utf8');
-    return hash.digest('hex');
-  }
-
-  function stringToBuffer(input: string) {
-    const buffer = Buffer.from(input, 'utf8');
-    return Buffer.concat([buffer, Buffer.from('00', 'hex')]);
-  }
-
-  function byteToBuffer(input: number) {
-    const buffer = Buffer.alloc(1);
-    buffer[0] = input;
-    return buffer;
   }
 }
