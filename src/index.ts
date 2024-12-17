63c523faef184c2e1408b5d2d4f350edff6bb4bd
Sadzurami <sadzurami2036@gmail.com>
2024-12-17 16:55:31 +0300

    feat: restructure core modules and interfaces for better organization

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import * as concurrency from './concurrency';
-import * as proc from './process';
-import * as resources from './resources';
-import * as steam from './steam';
+import * as concurrency from './core/concurrency';
+import * as proc from './core/process';
+import * as resources from './core/resources';
+import * as steam from './core/steam';
 
 export { proc as process, concurrency, resources, steam };
