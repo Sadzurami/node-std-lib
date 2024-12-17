96cceb46266697cd85d4e5701d87752866a9b370
Sadzurami <sadzurami2036@gmail.com>
2024-12-17 16:17:41 +0300

    feat: add index module to export core functionalities

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import * as concurrency from './concurrency';
+import * as proc from './process';
+import * as resources from './resources';
+import * as steam from './steam';
+
+export { proc as process, concurrency, resources, steam };
