4b90ad381e3c2d0e0f666b110dbe8204fc3b2ee9
Sadzurami <sadzurami2036@gmail.com>
2024-12-18 13:36:12 +0300

    refactor: remove unused exports from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-import * as concurrency from './core/concurrency';
-import * as proc from './core/process';
-import * as resources from './core/resources';
-import * as steam from './core/steam';
-
-export { proc as process, concurrency, resources, steam };
+export {};
